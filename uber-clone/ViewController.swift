4f6b923c929b7338359fc1843829ba3c5b1b701e
Ted.H <50784573+cielgrisdemoscou@users.noreply.github.com>
2020-10-18 13:59:05 +0900

    initial commit

---
+++ uber-clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  uber-clone
+//
+//  Created by Ted Hyeong on 18/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
